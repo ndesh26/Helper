3ca95c987ffc470016be8d7d407b026393add729
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2016-11-03 12:17:41 +0530

    scripts: Fix the grub-init script
    it was assuming the system to be BIOS but mine is UEFI so
    the location of grub.cfg will be different
    
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

--- scripts/grub-init.sh
+++ scripts/grub-init.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-source "term-colors"
-
 DISTRIBUTIVE=$(cat /etc/*-release | grep NAME | head -1 | sed -n -e 's/NAME\=//p')
 echo -e "Distributive: ${Green}${DISTRIBUTIVE}${Color_Off}"
 
 if [[ "$DISTRIBUTIVE" == "Fedora" ]] ;
 then
-    su -c 'grub2-mkconfig -o /boot/grub2/grub.cfg'
+    su -c 'grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg'
 else
     sudo update-grub
 fi
